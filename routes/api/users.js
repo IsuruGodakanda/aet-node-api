73b7fc30d7cef4a6047fba78e8be361293287faa
Isuru <isuru.g@aeturnum.com>
2021-01-21 23:52:04 +0530

    Initial Commit

--- routes/api/users.js
+++ routes/api/users.js
@@ -98,10 +98,94 @@ router.get('/', auth, async (req, res) => {
     let limit = parseInt(req.query.limit) || 5
     let search = req.query.search_term || ''
 
-    const users = await User.find({name: { $regex: `^${search}.*`  }}, {id: 1, name: 1, email: 1, role: 1}).skip(offset*limit).limit(limit);
+    const users = await User.find({name: { $regex: new RegExp("^" + search.toLowerCase(), "i")  }}, {id: 1, name: 1, email: 1, role: 1}).skip(offset*limit).limit(limit);
 
-    const userCount = await User.find({name: { $regex: `^${search}.*`  }}).countDocuments();
+    const userCount = await User.find({name: { $regex: new RegExp("^" + search.toLowerCase(), "i")  }}).countDocuments();
     res.json({"results": users, "totalCount": userCount});
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    GET api/users/:user_id
+// @desc     Get user by user ID
+// @access   Private
+router.get('/:user_id', auth, async (req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.params.user_id
+    }, {name: 1, email: 1, role: 1});
+
+    if (!user) return res.status(400).json({ msg: 'User not found' });
+
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'User not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    PUT api/users/:user_id
+// @desc     Update user by user ID
+// @access   Private
+router.put(
+  '/:user_id',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('role', 'Role is required')
+      .not()
+      .isEmpty(),
+  ], auth, async (req, res) => {  
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, role } = req.body;
+    try {
+      const user = await User.findOne({
+        _id: req.params.user_id
+      }, {name: 1, email: 1, role: 1});
+
+      if (!user) return res.status(400).json({ msg: 'User not found' });
+
+      await User.findOneAndUpdate(
+        { _id : req.params.user_id },
+        { $set: { name, email, role} },
+        { upsert:true, new : true }
+      );
+  
+      res.json({ msg: 'User updated' });
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind == 'ObjectId') {
+        return res.status(400).json({ msg: 'User not found' });
+      }
+      res.status(500).send('Server Error');
+    }
+});
+
+// @route    DELETE api/users/:user_id
+// @desc     Delete user by id
+// @access   Private
+router.delete('/:user_id', auth, async (req, res) => {
+  try {
+    const user = await User.findOne({
+      _id: req.params.user_id
+    }, {name: 1, email: 1, role: 1});
+
+    if (!user) return res.status(400).json({ msg: 'User not found' });
+
+    await User.findOneAndRemove({ _id: req.params.user_id });
+
+    res.json({ msg: 'User deleted' });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token');
+  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, DELETE, OPTIONS');
   next();
 });
 
